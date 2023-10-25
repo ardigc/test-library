33fea2fc354be61302f1a40498025c1e7488193a
ardigc <agordo.95@gmail.com>
2023-10-25 19:43:54 +0200

    async test

--- src/stats.test.ts
+++ src/stats.test.ts
@@ -1,10 +1,30 @@
 import { average, sum } from "./stats";
-import { expect } from "./testLib";
-let result, expected
- result = sum([1, 2 , 3])
- expected = 6
- expect(expected).toBe(result)
- 
- result = average([1, 2 , 3])
- expected = 2
- expect(expected).toBe(result)
+import { describe, expect, it, test } from "./testLib";
+import * as statsAsync from "./statsAsync";
+
+describe("Stats should", () => {
+  it("calculate sum", () => {
+    const result = sum([1, 2, 3]);
+    const expected = 6;
+    expect(expected).toBe(result);
+  });
+
+  it("Calculate average", () => {
+    const result = average([1, 2, 3]);
+    const expected = 2;
+    expect(expected).toBe(result);
+  });
+});
+describe("Stats Async should", () => {
+  it("calculate (async) sum", async () => {
+    const result = await statsAsync.sum([1, 2, 3]);
+    const expected = 6;
+    expect(expected).toBe(result);
+  });
+
+  it("Calculate (async) average", async () => {
+    const result = await statsAsync.average([1, 2, 3]);
+    const expected = 2;
+    expect(expected).toBe(result);
+  });
+});
