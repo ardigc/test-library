be5edc04f7b32f9a1b8b948415bee7a829981c01
ardigc <agordo.95@gmail.com>
2023-10-27 09:53:53 +0200

    add spyon

--- src/test/calculator.test.ts
+++ src/test/calculator.test.ts
@@ -1,17 +1,20 @@
 import * as arithmetic from "../Arithmetic";
 import * as calculator from "../calculate";
-jest.mock('../Arithmetic')
+// jest.mock('../Arithmetic')
 describe('The calculator', () => {
     // (arithmetic as any).add = jest.fn(arithmetic.add);
     // (arithmetic as any).subtract = jest.fn(arithmetic.subtract);
     it('calls arithmetic add', () => {
+        const mockedAdd= jest.spyOn(arithmetic,'add')
       const result=  calculator.doAdd(1, 2)
-        expect(arithmetic.add).toHaveBeenCalledWith(1, 2)
-        // expect(result).toBe(3)
+      expect(mockedAdd).toHaveBeenCalledWith(1, 2)
+        expect(result).toBe(3)
     })
     it('calls arithmetic subtract', () => {
+        const mockedSubtract= jest.spyOn(arithmetic,'subtract')
+        mockedSubtract.mockImplementation(()=>34)
         const result= calculator.doSubtract(1, 2)
-        expect(arithmetic.subtract).toHaveBeenCalledWith(1, 2)
-        // expect(result).toBe(-1)
+        expect(mockedSubtract).toHaveBeenCalledWith(1, 2)
+        expect(result).toBe(34)
     })
 })
