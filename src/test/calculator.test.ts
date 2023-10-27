46b1f8eb9559b5a304069e5ce05074182d9de345
ardigc <agordo.95@gmail.com>
2023-10-27 09:44:53 +0200

    jest.mock

--- src/test/calculator.test.ts
+++ src/test/calculator.test.ts
@@ -1,16 +1,17 @@
 import * as arithmetic from "../Arithmetic";
 import * as calculator from "../calculate";
+jest.mock('../Arithmetic')
 describe('The calculator', () => {
-    (arithmetic as any).add = jest.fn(arithmetic.add);
-    (arithmetic as any).subtract = jest.fn(arithmetic.subtract);
+    // (arithmetic as any).add = jest.fn(arithmetic.add);
+    // (arithmetic as any).subtract = jest.fn(arithmetic.subtract);
     it('calls arithmetic add', () => {
       const result=  calculator.doAdd(1, 2)
         expect(arithmetic.add).toHaveBeenCalledWith(1, 2)
-        expect(result).toBe(3)
+        // expect(result).toBe(3)
     })
     it('calls arithmetic subtract', () => {
         const result= calculator.doSubtract(1, 2)
         expect(arithmetic.subtract).toHaveBeenCalledWith(1, 2)
-        expect(result).toBe(-1)
+        // expect(result).toBe(-1)
     })
 })
