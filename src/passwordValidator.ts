d5df12f1e4fe8146124fe16aefe096592e0af73f
ardigc <agordo.95@gmail.com>
2023-11-03 15:01:19 +0100

    refactor

--- src/passwordValidator.ts
+++ src/passwordValidator.ts
@@ -1,8 +1,11 @@
 export const isStrongPassword = (pass: string) => {
-return hasSixCharactersOrMore(pass)&&containsNumber(pass)&& containsLowecase(pass)&&containsUppercase(pass)&&pass.includes('_')
+return hasSixCharactersOrMore(pass)&&containsNumber(pass)&& containsLowecase(pass)&&containsUppercase(pass)&&containsUnderscore(pass)
 }
 const containsNumber=(pass:string)=>{
     return /.*\d.*/.test(pass)
+}
+const containsUnderscore=(pass:string)=>{
+    return pass.includes('_')
 }
 const containsLowecase=(pass:string)=>{
     return /.*[a-z].*./.test(pass)
