3f9b75e6c094fc45f1733e82b7ab7b0a8399be14
ardigc <agordo.95@gmail.com>
2023-11-03 13:45:15 +0100

    fails when password is missing a number

--- src/test/passwordValidator.test.ts
+++ src/test/passwordValidator.test.ts
@@ -3,7 +3,11 @@ describe('The password strength validator', () => {
     it('considers a password to be strong when all requirements are met', () => {
         expect(isStrongPassword('1234abcdABCD_')).toBe(true)
     })
-    it('considers a password is too short', () => {
+    it('fails when password is too short', () => {
         expect(isStrongPassword('1aA_')).toBe(false)
     })
+    it('fails when password is missing a number', () => {
+        expect(isStrongPassword('abcdABCD_')).toBe(false)
+    })
+
 })
