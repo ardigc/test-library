f3645aa8781964f5ece169b3a9a3b6b04c200ff2
ardigc <agordo.95@gmail.com>
2023-11-06 16:54:42 +0100

    red

--- src/test/csvFilter.test.ts
+++ src/test/csvFilter.test.ts
@@ -2,9 +2,16 @@ import { CsvFilter } from "../CsvFilter";
 describe('CSV filter', ()=>{
     it('allow for correct lines only',()=>{
         const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
-        const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+        const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
     const csvFilter = CsvFilter.create([header, invoiceLine])
     const result = csvFilter.filteredLines
     expect(result).toEqual([header,invoiceLine])
+    })
+    it('excludes lines with both tax fields populated as the are exclusive',()=>{
+        const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+        const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+    const csvFilter = CsvFilter.create([header, invoiceLine])
+    const result = csvFilter.filteredLines
+    expect(result).toEqual([header])
     })
 })
