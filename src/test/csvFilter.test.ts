afc0c18d8d8c7505a40f7ec15737be7c887d5315
ardigc <agordo.95@gmail.com>
2023-11-06 17:09:50 +0100

    red

--- src/test/csvFilter.test.ts
+++ src/test/csvFilter.test.ts
@@ -21,9 +21,14 @@ describe('CSV Filter', () => {
     it('excludes lines with both tax field empty as one is required', () => {
       const invoiceLine = '1,02/05/2021,1000,790,,,ACER Laptop,B76430134,';
       const csvFilter = CsvFilter.create([header, invoiceLine]);
-        
       const result = csvFilter.filteredLines;
-        
+      expect(result).toEqual([header]);
+    });
+
+    it('excludes lines with non decimal tax fields', () => {
+      const invoiceLine = '1,02/05/2021,1000,790,jle,,ACER Laptop,B76430134,';
+      const csvFilter = CsvFilter.create([header, invoiceLine]);
+      const result = csvFilter.filteredLines;
       expect(result).toEqual([header]);
     });
   });
