bb260910a02f258c4332b483b506926d39ac3489
ardigc <agordo.95@gmail.com>
2023-11-06 17:01:51 +0100

    green

--- src/CsvFilter.ts
+++ src/CsvFilter.ts
@@ -1,9 +1,17 @@
-export class CsvFilter{
-   private constructor(private readonly lines:string[]){}
-    static create(lines:string[]){
-        return new CsvFilter(lines)
+export class CsvFilter {
+    private constructor(private readonly lines: string[]) {}
+    
+    static create(lines: string[]) {
+      return new CsvFilter(lines);
     }
-    get filteredLines(){
-        return this.lines
+    
+    get filteredLines() {
+      const result = [];
+      result.push(this.lines[0]);
+      const fields = this.lines[1].split(',');
+      if (!fields[4] || !fields[5]) {
+        result.push(this.lines[1]);
+      }
+    return result;
     }
-}
+  }
--- src/test/csvFilter.test.ts
+++ src/test/csvFilter.test.ts
@@ -1,17 +1,22 @@
 import { CsvFilter } from "../CsvFilter";
-describe('CSV filter', ()=>{
-    it('allow for correct lines only',()=>{
-        const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
-        const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
-    const csvFilter = CsvFilter.create([header, invoiceLine])
-    const result = csvFilter.filteredLines
-    expect(result).toEqual([header,invoiceLine])
-    })
-    it('excludes lines with both tax fields populated as the are exclusive',()=>{
-        const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
-        const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
-    const csvFilter = CsvFilter.create([header, invoiceLine])
-    const result = csvFilter.filteredLines
-    expect(result).toEqual([header])
-    })
-})
+describe('CSV Filter', () => {
+    it('Allow for correct lines only', () => {
+      const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+      const invoiceLine = '1,02/05/2021,1000,790,21,,ACER Laptop,B76430134,';
+      const csvFilter = CsvFilter.create([header, invoiceLine]);
+      
+      const result = csvFilter.filteredLines;
+      
+      expect(result).toEqual([header, invoiceLine]);
+    });
+          
+    it('excludes lines with both tax fields populated as they are exclusive', () => {
+      const header = 'Num_factura, Fecha, Bruto, Neto, IVA, IGIC, Concepto, CIF_cliente, NIF_cliente';
+      const invoiceLine = '1,02/05/2021,1000,790,21,7,ACER Laptop,B76430134,';
+      const csvFilter = CsvFilter.create([header, invoiceLine]);
+        
+      const result = csvFilter.filteredLines;
+        
+      expect(result).toEqual([header]);
+    });
+  });
