64ed2ed09443eb5b925b1f261eb74abb751396a6
ardigc <agordo.95@gmail.com>
2023-11-07 12:03:00 +0100

    refactor

--- src/primeFactors.ts
+++ src/primeFactors.ts
@@ -1,9 +1,12 @@
 export function getPrimesFactor(number: number) {
+    let prime = findSmaller(number)
+    const remainder = number / prime
+    return remainder <= 1 ? [prime] : [prime].concat(getPrimesFactor(remainder))
+}
+function findSmaller(number: number) {
     let factor = 2
     while (number % factor != 0) {
         ++factor
     }
-    const remainder = number / factor
-    return remainder <= 1 ? [factor] : [factor].concat(getPrimesFactor(remainder))
-
+    return factor
 }
