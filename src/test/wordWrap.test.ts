46ef874c4f07f4617588edabc9861ac0afa04b83
ardigc <agordo.95@gmail.com>
2023-11-07 12:24:05 +0100

    red

--- src/test/wordWrap.test.ts
+++ src/test/wordWrap.test.ts
@@ -4,5 +4,6 @@ return text
 describe('The word wrap ',()=>{
   it('makes every single line of test fit column width',()=>{
     expect(wordWrap('hello', 5)).toBe('hello')
+    expect(wordWrap('longword', 4)).toBe('long\nword')
   })  
 })
