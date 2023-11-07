9069990b48817aeed35f58d5c85e4c040f2920f6
ardigc <agordo.95@gmail.com>
2023-11-07 12:12:10 +0100

    green

--- src/primeFactors.ts
+++ src/primeFactors.ts
@@ -1,11 +1,12 @@
 export function getPrimesFactor(number: number) {
+    if(number<1) throw new Error("Only positive number are allowed");
+    
     let prime = findSmaller(number)
     const remainder = number / prime
     return remainder <= 1 ? [prime] : [prime].concat(getPrimesFactor(remainder))
 }
 function findSmaller(number: number) {
     if(number===1) return 1
-
     let factor = 2
     while (number % factor != 0) {
         ++factor
