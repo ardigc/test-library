ad06fe5d69f22fbfb9072cc8571f9c29946c3695
ardigc <agordo.95@gmail.com>
2023-11-07 12:57:42 +0100

    green

--- src/test/wordWrap.test.ts
+++ src/test/wordWrap.test.ts
@@ -1,15 +1,25 @@
-function wordWrap(text: string, columnWidht: number) {
-    if (text.length <= columnWidht) {
-        return text
-    }
-    const wrappedText = text.substring(0, columnWidht) + '\n'
-    const unWrappedText = text.substring(columnWidht)
-    return wrappedText.concat(wordWrap(unWrappedText, columnWidht))
+function wordWrap(text: string, columnWidth: number) {
+  if (text.length <= columnWidth) {
+    return text;
+  }
+  let wrappedText;
+  let unwrappedText;
+  if (text.indexOf(' ') > -1 && text.indexOf(' ') < columnWidth) {
+    wrappedText = text.substring(0, text.indexOf(' ')).concat('\n');
+    unwrappedText = text.substring(text.indexOf(' ') + 1);
+  } else {
+    wrappedText = text.substring(0, columnWidth).concat('\n');
+    unwrappedText = text.substring(columnWidth);
+  }
+  return wrappedText.concat(wordWrap(unwrappedText, columnWidth));
 }
 describe('The word wrap ', () => {
     it('makes every single line of test fit column width', () => {
         expect(wordWrap('hello', 5)).toBe('hello')
         expect(wordWrap('longword', 4)).toBe('long\nword')
         expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd')
+        expect(wordWrap('abc def', 4)).toBe('abc\ndef');
+        expect(wordWrap('abc def ghi', 4)).toBe('abc\ndef\nghi');
+        expect(wordWrap(' abcd', 4)).toBe('\nabcd');
     })
 })
