bd719d5195353bad0ef6ab284b133f57ff83350a
ardigc <agordo.95@gmail.com>
2023-11-07 12:30:30 +0100

    green

--- src/test/wordWrap.test.ts
+++ src/test/wordWrap.test.ts
@@ -4,11 +4,12 @@ function wordWrap(text: string, columnWidht: number) {
     }
     const wrappedText = text.substring(0, columnWidht) + '\n'
     const unWrappedText = text.substring(columnWidht)
-    return wrappedText + unWrappedText
+    return wrappedText.concat(wordWrap(unWrappedText, columnWidht))
 }
 describe('The word wrap ', () => {
     it('makes every single line of test fit column width', () => {
         expect(wordWrap('hello', 5)).toBe('hello')
         expect(wordWrap('longword', 4)).toBe('long\nword')
+        expect(wordWrap('reallylongword', 4)).toBe('real\nlylo\nngwo\nrd')
     })
 })
