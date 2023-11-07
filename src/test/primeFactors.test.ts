6c71818c65e8c1be046ab041bd7ca18cf14d31fe
ardigc <agordo.95@gmail.com>
2023-11-07 11:57:34 +0100

    refactor

--- src/test/primeFactors.test.ts
+++ src/test/primeFactors.test.ts
@@ -1,23 +1,15 @@
 import { getPrimesFactor } from "../primeFactors";
-/*
-2 -> [2]
-2 * 2 -> [2,2]
-2 * 2 * 2 -> [2,2,2]
-3 -> [3]
-3 * 3 -> [3,3]
-3 * 2 -> [2,3]
-5 * 5 -> [5,5]
-5 * 7 * 11 * 3 -> [3,5,7,11]
-*/
+
 describe('The prime factors',()=>{
-    it('finds the prime comopsition of the given number',()=>{
+
+    it('knows what is  a prime number',()=>{
         expect(getPrimesFactor(2)).toEqual([2])
-        expect(getPrimesFactor(2*2)).toEqual([2,2])
-        expect(getPrimesFactor(2*2*2)).toEqual([2,2,2])
         expect(getPrimesFactor(3)).toEqual([3])
-        expect(getPrimesFactor(2*3)).toEqual([2,3])
-        expect(getPrimesFactor(3*3)).toEqual([3,3])
-        expect(getPrimesFactor(5*5)).toEqual([5,5])
+    })
+    it('should the same result to multiply the numbers in the output list',()=>{
+        expect(getPrimesFactor(2*2*2)).toEqual([2,2,2])
+    })
+    it('orders the prime factors from the smallest to the biggest',()=>{
         expect(getPrimesFactor(5 * 7 * 11 * 3)).toEqual([3,5,7,11])
     })
 })
