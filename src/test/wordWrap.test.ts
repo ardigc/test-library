24850258fb95957fed12a29048ffe5fe5e5b679b
ardigc <agordo.95@gmail.com>
2023-11-07 12:28:01 +0100

    refactor

--- src/test/wordWrap.test.ts
+++ src/test/wordWrap.test.ts
@@ -1,9 +1,14 @@
-function wordWrap(text: string, columnWidht:number) {
-return text
+function wordWrap(text: string, columnWidht: number) {
+    if (text.length <= columnWidht) {
+        return text
+    }
+    const wrappedText = text.substring(0, columnWidht) + '\n'
+    const unWrappedText = text.substring(columnWidht)
+    return wrappedText + unWrappedText
 }
-describe('The word wrap ',()=>{
-  it('makes every single line of test fit column width',()=>{
-    expect(wordWrap('hello', 5)).toBe('hello')
-    expect(wordWrap('longword', 4)).toBe('long\nword')
-  })  
+describe('The word wrap ', () => {
+    it('makes every single line of test fit column width', () => {
+        expect(wordWrap('hello', 5)).toBe('hello')
+        expect(wordWrap('longword', 4)).toBe('long\nword')
+    })
 })
