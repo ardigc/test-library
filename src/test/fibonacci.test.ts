32044cdf0c000781545726f72d9bdd2831339e13
ardigc <agordo.95@gmail.com>
2023-11-07 10:40:30 +0100

    refactor

--- src/test/fibonacci.test.ts
+++ src/test/fibonacci.test.ts
@@ -7,9 +7,7 @@ describe('The fibonacci sequence', () => {
         expect(fibonacci(1)).toBe(1)
     })
     it('is a series where the value for a number is de addition of the preceding two values', () => {
-        expect(fibonacci(2)).toBe(fibonacci(0)+fibonacci(1))
-        expect(fibonacci(3)).toBe(fibonacci(1)+fibonacci(2))
-        expect(fibonacci(4)).toBe(fibonacci(2)+fibonacci(3))
-        expect(fibonacci(5)).toBe(fibonacci(3)+fibonacci(4))
+        [2,3,4,5].forEach((n)=>expect(fibonacci(n)).toBe(fibonacci(n-1)+fibonacci(n-2)))
+
     })
 })
